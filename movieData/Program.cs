7e9af7afb593a655180bde8bcfaaed24eeb64907
rzhou10 <rzhou10@gmail.com>
2018-10-08 08:58:40 -0400

    First Commit

---
+++ movieData/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace movieData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application xlApp = new Excel.Application();
+
+            Excel.Workbook movieWorkbook = xlApp.Workbooks.Open(@"C:\Users\Class2018\source\repos\movieData\movieData\Movie Data.xlsx");
+            Excel._Worksheet xlWorksheet = movieWorkbook.Sheets[1];
+
+            Excel.Range titleCol = xlWorksheet.Columns[1];
+            Excel.Range yearCol = xlWorksheet.Columns[2];
+            Excel.Range genreCol = xlWorksheet.Columns[3];
+            Excel.Range dirCol = xlWorksheet.Columns[4];
+            Excel.Range runCol = xlWorksheet.Columns[5];
+            Excel.Range grossCol = xlWorksheet.Columns[6];
+            Excel.Range ratingCol = xlWorksheet.Columns[7];
+
+            System.Array listVals = (System.Array)titleCol.Cells.Value;
+            string[] allTitles = listVals.OfType<object>().Select(o => o.ToString()).ToArray();
+
+            System.Array listVals1 = (System.Array)yearCol.Cells.Value;
+            string[] allYears = listVals1.OfType<object>().Select(o => o.ToString()).ToArray();
+
+            System.Array listVals2 = (System.Array)genreCol.Cells.Value;
+            string[] allGenre = listVals2.OfType<object>().Select(o => o.ToString()).ToArray();
+
+            System.Array listVals3 = (System.Array)dirCol.Cells.Value;
+            string[] allDir = listVals3.OfType<object>().Select(o => o.ToString()).ToArray();
+
+            System.Array listVals4 = (System.Array)runCol.Cells.Value;
+            string[] allRun = listVals4.OfType<object>().Select(o => o.ToString()).ToArray();
+
+            System.Array listVals5 = (System.Array)grossCol.Cells.Value;
+            string[] allGross = listVals5.OfType<object>().Select(o => o.ToString()).ToArray();
+
+            System.Array listVals6 = (System.Array)ratingCol.Cells.Value;
+            string[] allRating = listVals6.OfType<object>().Select(o => o.ToString()).ToArray();
+
+            Stats s = new Stats();
+
+            Dictionary<string, int> DictYears = s.AddYears(allYears);
+            Dictionary<string, int> DictGenres = s.AddGenres(allGenre);
+            Dictionary<string, int> DictDirs = s.AddDirectors(allDir);
+            Dictionary<string, int> DictRating = s.AddGenres(allRating);
+
+            s.SetGrossStats(allGross, allTitles);
+            s.SetRunStats(allRun, allTitles);
+
+            //write to excel sheets
+
+            Excel._Worksheet xlWorksheet2 = movieWorkbook.Sheets[2];
+            xlWorksheet2.Cells[1, 1] = "Directors";
+            xlWorksheet2.Cells[1, 2] = "Count";
+
+            int a = 1;
+            foreach (var d in DictDirs)
+            {
+                a++;
+                xlWorksheet2.Cells[a, 1] = d.Key;
+                xlWorksheet2.Cells[a, 2] = d.Value;
+            }
+
+            Excel._Worksheet xlWorksheet3 = movieWorkbook.Sheets[3];
+            xlWorksheet3.Cells[1, 1] = "Years";
+            xlWorksheet3.Cells[1, 2] = "Count";
+
+            int b = 1;
+            foreach (var y in DictYears)
+            {
+                b++;
+                xlWorksheet3.Cells[b, 1] = y.Key;
+                xlWorksheet3.Cells[b, 2] = y.Value;
+            }
+
+            Excel._Worksheet xlWorksheet4 = movieWorkbook.Sheets[4];
+            xlWorksheet4.Cells[1, 1] = "Genre";
+            xlWorksheet4.Cells[1, 2] = "Count";
+
+            int c = 1;
+            foreach (var g in DictGenres)
+            {
+                c++;
+                xlWorksheet4.Cells[c, 1] = g.Key;
+                xlWorksheet4.Cells[c, 2] = g.Value;
+            }
+
+            Excel._Worksheet xlWorksheet5 = movieWorkbook.Sheets[5];
+            xlWorksheet5.Cells[1, 1] = "Rating";
+            xlWorksheet5.Cells[1, 2] = "Count";
+
+            int e = 1;
+            foreach (var r in DictRating)
+            {
+                e++;
+                xlWorksheet5.Cells[e, 1] = r.Key;
+                xlWorksheet5.Cells[e, 2] = r.Value;
+            }
+
+            Console.WriteLine("Done!");
+
+            Marshal.ReleaseComObject(yearCol);
+            Marshal.ReleaseComObject(genreCol);
+            Marshal.ReleaseComObject(dirCol);
+            Marshal.ReleaseComObject(runCol);
+            Marshal.ReleaseComObject(grossCol);
+            Marshal.ReleaseComObject(ratingCol);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            movieWorkbook.Close();
+            Marshal.ReleaseComObject(movieWorkbook);
+
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+    }
+}
---
+++ movieData/Stats.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace movieData
+{
+    class Stats
+    {
+        private BigInteger GrossAvg { get; set; }
+        private int MaxGross { get; set; }
+        private string HighestFilm { get; set; }
+        private string LowestFilm { get; set; }
+        private int MinGross { get; set; }
+        private double RunAvg { get; set; }
+        private string LongestFilm { get; set; }
+        private string ShortestFilm { get; set; }
+        private int MaxRun { get; set; }
+        private int MinRun { get; set; }
+
+        public Dictionary<string, int> AddDirectors(string[] arr)
+        {
+            Dictionary<string, int> directors = new Dictionary<string, int>();
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (directors.ContainsKey(arr[i]))
+                {
+                    directors[arr[i]] = directors[arr[i]] += 1;
+                }
+                else
+                {
+                    directors.Add(arr[i], 1);
+                }
+            }
+
+            return directors;
+        }
+
+        public Dictionary<string, int> AddGenres(string[] arr)
+        {
+            Dictionary<string, int> genre = new Dictionary<string, int>();
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (genre.ContainsKey(arr[i]))
+                {
+                    genre[arr[i]] = genre[arr[i]] += 1;
+                }
+                else
+                {
+                    genre.Add(arr[i], 1);
+                }
+            }
+
+            return genre;
+        }
+
+        public Dictionary<string, int> AddYears(string[] arr)
+        {
+            Dictionary<string, int> years = new Dictionary<string, int>();
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (years.ContainsKey(arr[i]))
+                {
+                    years[arr[i]] = years[arr[i]] += 1;
+                }
+                else
+                {
+                    years.Add(arr[i], 1);
+                }
+            }
+
+            return years;
+        }
+
+        public Dictionary<string, int> AddRatings(string[] arr)
+        {
+            Dictionary<string, int> ratings = new Dictionary<string, int>();
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (ratings.ContainsKey(arr[i]))
+                {
+                    ratings[arr[i]] = ratings[arr[i]] += 1;
+                }
+                else
+                {
+                    ratings.Add(arr[i], 1);
+                }
+            }
+
+            return ratings;
+        }
+
+        public void SetGrossStats(string[] arr, string[] arr1)
+        {
+            BigInteger sum = 0;
+            int NON_NA = 0;
+            MinGross = Int32.MaxValue;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] != "N/A")
+                {
+                    int ValToString = System.Convert.ToInt32(arr[i]);
+                    sum += ValToString;
+                    NON_NA++;
+
+                    if (ValToString > MaxGross)
+                    {
+                        MaxGross = ValToString;
+                        HighestFilm = arr1[i];
+                        
+                    }
+                    else if (ValToString < MinGross)
+                    {
+                        MinGross = ValToString;
+                        LowestFilm = arr1[i];
+                    }
+                }
+            }
+
+            GrossAvg = sum / NON_NA;
+        }
+
+        public BigInteger GetGrossAvg()
+        {
+            return GrossAvg;
+        }
+
+        public int GetMaxGross()
+        {
+            return MaxGross;
+        }
+
+        public int GetMinGross()
+        {
+            return MinGross;
+        }
+
+        public string GetHighestFilm()
+        {
+            return HighestFilm;
+        }
+
+        public string GetLowestFilm()
+        {
+            return LowestFilm;
+        }
+
+        public void SetRunStats(string[] arr, string[] arr1)
+        {
+            int sum = 0;
+            MinRun = Int32.MaxValue;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int ValToString = System.Convert.ToInt32(arr[i]);
+                sum += ValToString;
+
+                if (ValToString > MaxRun)
+                {
+                    MaxRun = ValToString;
+                    LongestFilm = arr1[i];
+                }
+                else if (ValToString < MinRun)
+                {
+                    MinRun = ValToString;
+                    ShortestFilm = arr1[i];
+                }
+            }
+
+            RunAvg = sum / arr.Length;
+        }
+
+        public double GetRunAvg()
+        {
+            return RunAvg;
+        }
+
+        public int GetMaxRun()
+        {
+            return MaxRun;
+        }
+
+        public int GetMinRun()
+        {
+            return MinRun;
+        }
+
+        public string GetLongestFilm()
+        {
+            return LongestFilm;
+        }
+
+        public string GetShortestFilm()
+        {
+            return ShortestFilm;
+        }
+    }
+}
